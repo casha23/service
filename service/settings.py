86a01bdd345ce9e6ca909c98bb61b69d6ade2b8c
casha23 <11davidg99@gmail.com>
2023-01-20 21:10:46 +0200

    #4 Setup database

--- service/settings.py
+++ service/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'service_centre',
+        'USER': 'service_centre_user',
+        'PASSWORD': '123456789',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
